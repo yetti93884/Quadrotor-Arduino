44fa9727b6a864a80bb9327587489005b1a294f3
atulya <atulyarox@gmail.com>
2014-06-21 19:07:51 +0530

    UPdated the control code and added ArduIMU code

--- quad_main_code/quad_config.h
+++ quad_main_code/quad_config.h
@@ -1,13 +1,24 @@
 #ifndef QUAD_CONFIG_H
 #define QUAD_CONFIG_H
 
-#define MOTOR_LEFT_PIN 8
-#define MOTOR_RIGHT_PIN 9
-#define MOTOR_FRONT_PIN 11
-#define MOTOR_BACK_PIN 10
+#define MOTOR_LEFT_PIN 8  //clockwise
+#define MOTOR_RIGHT_PIN 9  //clockwise
+#define MOTOR_FRONT_PIN 11  //anti-clockwise
+#define MOTOR_BACK_PIN 10  //anti-clockwise
 
-#define MOTOR_PWM_MIN 800
-#define MOTOR_PWM_MAX 2500
+#define MOTOR_PWM_MIN 900
+#define MOTOR_ARM_PWM 800
+#define MOTOR_PWM_MAX 1300
+
+
+#define JOYSTICK_START '$'
+#define JOYSTICK_END ':'
+
+#define MESSAGE_START '%'
+#define MESSAGE_END ':'
+
+#define EMERGENCY_STOP '!'
+#define CONTROL_RESTART '?'
 
 /* set the settings for the IMU here*/
 void initializeIMU();
@@ -18,8 +29,20 @@ void initizeMotors();
 /* updates the PWM to each of the BLDC motors*/
 void updateMotors();
 
+/* STOPs the action of all the motors by setting them to minimum PWM*/
+void stopMotor();
+
 /* transforms the throttle value to motor PWM for all motors*/
 void throttleToMotor();
+
+/* takes input from Serial3 port and classifies it as either Joystick
+or Message input*/
+void parseSerialInput();
+
+/* A message can be taken from the user in the form of "FL 1500" for 
+sending a particular message and an argument from Serial3. This 
+function reads the messaae and takes an action*/
+void parseMessage();
 
 /* takes the joystick input on Serial3 and writes it to command_roll,
 command_pitch, command_thrust*/
@@ -34,11 +57,26 @@ void printJoyStickInput();
 /* converts from the quarternion to euler angle notation*/
 void quaternionToEuler(float *q, float *euler);
 
+/* returns conjugate of a quaternion */
+void quatConjugate(float *quat, float *result);
+
+/* returns product of quaternions */
+void quatProd(float * a, float * b, float *result);
+
 /* prints to Serial port 0 the PWM value passed to each of the BLDC motors*/
 void printMotorPWM();
 
 /* prints the IMU  roll pitch yaw angles as computed by the IMU*/
 void printIMUReadings();
+
+/* Updates parameters used by control algo */
+void updateControlParams();
+
+/* Evaluates the control inputs U1, U2, U3, U4 */
+void executeController();
+
+/* Evaluates PWM signal based on U1, U2, U3, U4 */
+void getPWM();
 
 #endif
 
--- quad_main_code/quad_main_code.ino
+++ quad_main_code/quad_main_code.ino
@@ -1,36 +1,50 @@
 
+
 //#include <PinChangeInt.h>
 //#include <Timer.h>
 
 #include <Wire.h>
 #include <Servo.h>
+#include "Timer.h"
+
 #include "quad.h"
+
 #include "quad_config.h"
 
 void setup() {
   Serial.begin(57600);  // lappy
-  Serial2.begin(57600);  // imu
+  Serial2.begin(115200);  // imu
   Serial3.begin(57600);  // xbee
   Wire.begin();
-  delay(5);
+  
+  //delay(5);
   delay(5);
 
   initializeIMU();
   initializeMotors();
+  
 }
 
 void loop() {
   
   if (Serial3.available() > 0) {                              // Joystick reading available
-    parseJoyStickInput();
-    throttleToMotor();
+    parseSerialInput();
   }
   
-  if (Serial2.available() > 0) {                              // Xbee reading available
+  
+  if (Serial2.available() > 0) {                              // IMU reading available
     parseIMUInput();
   }
 
-  updateMotors();
+  if (Serial.available() > 0) {                               // Instructions from lappy
+    setIMUoffset();
+  }
+  
+  updateControlParams();                                      // Updates parameters used by control algo
+  executeController();                                        // Evaluates the control inputs U1, U2, U3, U4
+  getPWM();                                                   // Evaluates PWM signal based on U1, U2, U3, U4
+  updateMotors();                                             // Provides PWM signal
+  
   printJoyStickInput();
   printIMUReadings();
   printMotorPWM();
